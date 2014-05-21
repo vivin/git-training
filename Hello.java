36ec64d99f0566dc9cfc564fc9e520cfc7dd9912
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-21 11:20:38 -0700

    Adding changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("So nice to meet you!");
+        System.out.println("And you, and you, and you, and you, and you!");
     }
 }
