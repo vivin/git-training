ede4a821996e0598a683848aa167cffe49223ab6
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-22 09:16:09 -0700

    Revert "Adding changes to Hello.java"
    This reverts commit 36ec64d99f0566dc9cfc564fc9e520cfc7dd9912.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("So nice to meet you!");
-        System.out.println("And you, and you, and you, and you, and you!");
     }
 }
