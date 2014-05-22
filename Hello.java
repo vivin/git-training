aec9ba866bc4ee9093771028ef3974e0de9579bd
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-22 11:09:44 -0700

    "Hello, Goodbye"

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("You say goodbye and I say hello");
+        System.out.println("Hello hello");
+        System.out.println("I don't know why you say goodbye, I say hello");
     }
 }
