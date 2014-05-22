5a8c7d531985d9d1416aefa9bb78aec1481afea5
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-22 16:29:50 -0700

    French version of Hello

---
+++ Bonjour.java
@@ -0,0 +1,7 @@
+public class Bonjour {
+    public static void main(String[] args) {
+        System.out.println("Tu dis au revoir et je dit bonjour");
+        System.out.println("Bonjour bonjour");
+        System.out.println("Je ne sais pas pourquoi tu dis au revoir, et je dit bonjour");
+    }
+}
