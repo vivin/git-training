d0fff0a6e2ef40d6a27b55d51eb6617db0d5e62b
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-23 10:51:02 -0700

    Added some punctuation to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("You say goodbye and I say hello");
-        System.out.println("Hello hello");
-        System.out.println("I don't know why you say goodbye, I say hello");
+        System.out.println("You say goodbye and I say hello!");
+        System.out.println("Hello! Hello!");
+        System.out.println("I don't know why you say goodbye, I say hello!");
 
         System.out.println("I say high, you say low");
-        System.out.println("You say why and I say I don't know, oh no");
-        System.out.println("You say goodbye and I say hello");
+        System.out.println("You say why and I say I don't know, oh no!");
+        System.out.println("You say goodbye and I say hello!");
     }
 }
