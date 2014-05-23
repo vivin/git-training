3fc6467ea3121c02ae10e9a77bdf47b516601e7b
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-23 14:28:46 -0700

    Adding spanish version of Hello.java

---
+++ Hola.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("¡Tú dices adiós y yo digo hola!");
+        System.out.println("¡Hola! ¡Hola!");
+        System.out.println("¡No sé por qué dices adiós, Yo digo hola!");
+    }
+}
+
