6a1c649698260958400145258b031d112fbecad7
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-23 14:34:23 -0700

    Adding some more lines to Goodbye.java

--- Goodbye.java
+++ Goodbye.java
@@ -1,5 +1,9 @@
 public class Goodbye {
     public static void main(String[] args) {
-        System.out.println("Goodbye, cruel world!");
+        System.out.println("Goodbye, cruel world");
+        System.out.println("I'm leaving you today");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
+        System.out.println("Goodbye");
     }
 }
