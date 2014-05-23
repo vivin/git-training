c01fe43b40309c54eac193e6e02117e0f9597fe9
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-23 09:36:59 -0700

    Some additional changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ public class Hello {
         System.out.println("You say goodbye and I say hello");
         System.out.println("Hello hello");
         System.out.println("I don't know why you say goodbye, I say hello");
+
+        System.out.println("I say high, you say low");
+        System.out.println("You say why and I say I don't know, oh no");
+        System.out.println("You say goodbye and I say hello");
     }
 }
