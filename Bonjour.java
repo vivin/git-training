c28d851c685f4f2760dbdb58aabca0a0d14106be
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-27 10:16:20 -0700

    Adding some changes to Bonjour.java
    Adding some more changes to Bonjour.java

--- Bonjour.java
+++ Bonjour.java
@@ -1,7 +1,11 @@
 public class Bonjour {
     public static void main(String[] args) {
-        System.out.println("Tu dis au revoir et je dit bonjour");
+        System.out.println("Tu dis au revoir et je dis bonjour");
         System.out.println("Bonjour bonjour");
-        System.out.println("Je ne sais pas pourquoi tu dis au revoir, et je dit bonjour");
+        System.out.println("Je ne sais pas pourquoi tu dis au revoir, et je dis bonjour");
+
+        System.out.println("Je dis haut, tu dis bas");
+        System.out.println("Tu dis pourquoi, et je dis je ne sais pas, oh non!");
+        System.out.println("Tu dis au revoir et je dis bonjour");
     }
 }
