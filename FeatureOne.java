17de8ee73f02576701af3e9a833ce24f4f7958a1
Vivin Paliath <vivin.paliath@gmail.com>
2014-05-29 11:36:40 -0700

    Feature one is the best

--- FeatureOne.java
+++ FeatureOne.java
@@ -2,5 +2,6 @@ public class FeatureOne {
     public static void main(String[] args) {
         System.out.println("Feature One");
         System.out.println("Feature One!");
+        System.out.println("This is the best feature ever!");
     }
 }
